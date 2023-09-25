d427dad3e42404fb229829c9eaac45e80cf5eff8
dawoodrizwan84 <dawood.rizwan@outlook.com>
2023-09-25 13:54:56 +0200

    Add project files.

---
+++ DEX_MovieShopProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEX_MovieShopProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
