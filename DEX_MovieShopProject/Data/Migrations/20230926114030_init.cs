bf306c368279feedc7c3bac571d47390c6b47d4e
dawoodrizwan84 <dawood.rizwan@outlook.com>
2023-09-26 13:41:14 +0200

    updated movie class

---
+++ DEX_MovieShopProject/Data/Migrations/20230926114030_init.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEX_MovieShopProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReleaseYear",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Disciption",
+                table: "Movies",
+                type: "int",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disciption",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReleaseYear",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
