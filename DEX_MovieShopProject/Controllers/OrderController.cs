3974219645c1aa527b89b3cc784cf6653df79fab
Ehsan Barekzai <ehsan.barekzai2000@gmail.com>
2023-10-04 09:31:31 +0200

    Ehsan Barekzai Orders Models Controllers and views work done

---
+++ DEX_MovieShopProject/Controllers/OrderController.cs
@@ -0,0 +1,151 @@
+﻿using DEX_MovieShopProject.Data;
+using DEX_MovieShopProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEX_MovieShopProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public OrderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            List<Order> orders = await _context.Orders.ToListAsync();
+            return View(orders);
+        }
+        [HttpGet]
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order Order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+
+            return View(Order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(Order Order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(Order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Order);
+        }
+
+        public IActionResult Edit()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order Order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+
+            return View(Order);
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Order Order)
+        {
+            if (id != Order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(Order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Order);
+        }
+        [HttpGet]
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order Order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+
+            return View(Order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Order Order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+            _context.Orders.Remove(Order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(o => o.Id == id);
+        }
+    }
+}
---
+++ DEX_MovieShopProject/Controllers/OrderRowsController.cs
@@ -0,0 +1,146 @@
+﻿using DEX_MovieShopProject.Data;
+using DEX_MovieShopProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEX_MovieShopProject.Controllers
+{
+    public class OrderRowsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public OrderRowsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: OrderRows
+        public async Task<IActionResult> Index()
+        {
+            List<OrderRow> orderRows = await _context.OrderRows.ToListAsync();
+            return View(orderRows);
+        }
+
+        // GET: OrderRows/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderRow orderRow = await _context.OrderRows.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (orderRow == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderRow);
+        }
+
+        // GET: OrderRows/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderRows/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(OrderRow orderRow)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderRow);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(orderRow);
+        }
+
+        // GET: OrderRows/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderRow orderRow = await _context.OrderRows.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (orderRow == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderRow);
+        }
+
+        // POST: OrderRows/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, OrderRow orderRow)
+        {
+            if (id != orderRow.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderRow);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderRowExists(orderRow.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(orderRow);
+        }
+
+        // GET: OrderRows/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderRow orderRow = await _context.OrderRows.FirstOrDefaultAsync(o => o.Id == id);
+
+            if (orderRow == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderRow);
+        }
+
+        // POST: OrderRows/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            OrderRow orderRow = await _context.OrderRows.FirstOrDefaultAsync(o => o.Id == id);
+            _context.OrderRows.Remove(orderRow);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderRowExists(int id)
+        {
+            return _context.OrderRows.Any(o => o.Id == id);
+        }
+    }
+}
