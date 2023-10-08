332c99dab88efaa73318197b1d99cb1e59a4ff01
Ehsan Barekzai <ehsan.barekzai2000@gmail.com>
2023-10-08 18:14:20 +0200

    initial

--- DEX_MovieShopProject/Controllers/HomeController.cs
+++ DEX_MovieShopProject/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using DEX_MovieShopProject.Models;
+﻿using DEX_MovieShopProject.Data;
+using DEX_MovieShopProject.Models;
 using DEX_MovieShopProject.Models.ViewModels;
 using DEX_MovieShopProject.Service.Abstract;
 using DEX_MovieShopProject.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DEX_MovieShopProject.Controllers
@@ -12,15 +14,27 @@ namespace DEX_MovieShopProject.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IMovieService _movieService;
        
-
-       
+        private readonly AppDbContext _appDbContext;
         private readonly ICustomerService _customerService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext dbcontext)
         {
             _logger = logger;
+            _appDbContext = dbcontext;
+           
         }
+        
+        public  IActionResult Search(string searchTitle)
+        {
 
+            var query  = _appDbContext.Movies.AsQueryable();
+            if (searchTitle!=null)
+            {
+                query = query.Where(m => m.Title.Contains(searchTitle));
+            }
+            var movies = query.ToList();
+            return View(movies);
+        }
         public IActionResult Index()
         {
 
@@ -44,13 +58,13 @@ namespace DEX_MovieShopProject.Controllers
 
             //frontPage.TopSellerMovies = movies;
 
-            return View(/*frontPage*/);
+            //return View(/*frontPage*/);
 
             //FrontPageVM frontPage = new FrontPageVM();
             //frontPage.CheapMovies = _movieService.GetMovies();
             //frontPage.TopFiveMovies = _movieService.GetMovies();
-
-            return View();
+            var movies  = _appDbContext.Movies.ToList();
+            return View(movies);
 
         }
 
